ce698c7688f43a2bc0cc5fefae6920da675ddc29
kira <137487691+sharkyrahh@users.noreply.github.com>
2025-09-01 04:25:51 +0800

    Sprint 2 Template Files
    Added
    -ViewParking
    -AddParking
    -EditParking
    -History
    -EntryFragment
    -ExitFragment

---
+++ SmartParkParkingSystem/app/src/main/java/com/example/smartparkparkingsystem/ui/dashboard/ParkingActivity.java
@@ -0,0 +1,26 @@
+package com.example.smartparkparkingsystem.ui.dashboard;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.smartparkparkingsystem.R;
+
+public class ParkingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_parking);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
