6e51e134d20b3fb4fdcc3e60adea28d464acc174
kira <137487691+sharkyrahh@users.noreply.github.com>
2025-09-08 21:53:54 +0800

    Added background & text color to uncolored
    Text is now visible when phone is in Dark Mode

--- SmartParkParkingSystem/app/src/main/java/com/example/smartparkparkingsystem/ui/history/HistoryFragment.java
+++ SmartParkParkingSystem/app/src/main/java/com/example/smartparkparkingsystem/ui/history/HistoryFragment.java
@@ -6,6 +6,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,49 +16,47 @@ import com.google.android.material.tabs.TabLayout;
 
 public class HistoryFragment extends Fragment {
 
-    private ViewPager viewPager; // Add this line
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    MyViewPagerAdapter myViewPagerAdapter;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_history, container, false);
-        viewPager = view.findViewById(R.id.pager); // Make sure this ID exists in your XML
+
+        tabLayout = view.findViewById(R.id.tab_layout);
+        viewPager2 = view.findViewById(R.id.view_pager);
+        myViewPagerAdapter = new MyViewPagerAdapter(this);
+        viewPager2.setAdapter(myViewPagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
+
         return view;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
-
-        // Setup ViewPager adapter
-        DemoCollectionPagerAdapter adapter = new DemoCollectionPagerAdapter(getChildFragmentManager());
-        viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
-    }
-
-    // Move the adapter class inside HistoryFragment but as a nested class
-    public static class DemoCollectionPagerAdapter extends FragmentStatePagerAdapter {
-
-        public DemoCollectionPagerAdapter(@NonNull androidx.fragment.app.FragmentManager fm) {
-            super(fm);
-        }
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-            // You'll need to create these fragment classes
-            // For now, return null or create simple fragments
-            return new Fragment(); // Replace with your actual fragments
-        }
-
-        @Override
-        public int getCount() {
-            return 4;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return "OBJECT " + (position + 1);
-        }
     }
 }
